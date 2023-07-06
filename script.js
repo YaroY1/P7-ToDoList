909b64bac288e63a269d05049e22ca85f73cc40b
Jarek B <jaroslaw.bielecki@poczta.fm>
2023-07-06 17:03:03 +0200

    last commit, todo list ready

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let addBtn; //dodaje nowe elementy do listy
 let ulList; //lista zadań
 let newTodo; //nowo dodane li/nowe zadanie
 
+let popup; //popup
+let popupInfo; //tekst w popupie, jak się doda pusty tekst
+let todoToEdit; //edytowany Todo
+let popupInput; //input w popupie
+let popupAddBtn; //przycisk "zatwierdź" w popupie
+let popupCloseBtn; //przycisk "anuluj" w popupie
+
 const main = () => {
 	prepareDOMElements();
 	prepareDOMEvents();
@@ -15,12 +22,21 @@ const prepareDOMElements = () => {
 	errorInfo = document.querySelector(".error-info");
 	addBtn = document.querySelector(".btn-add");
 	ulList = document.querySelector(".todoList ul");
+
+	popup = document.querySelector(".popup");
+	popupInfo = document.querySelector(".popup-info");
+	popupInput = document.querySelector(".popup-input");
+	popupAddBtn = document.querySelector(".accept");
+	popupCloseBtn = document.querySelector(".cancel");
 };
 
 const prepareDOMEvents = () => {
 	//nadawanie nasłuchiwania
 	addBtn.addEventListener("click", addNewTodo);
 	ulList.addEventListener("click", checkClick);
+	popupCloseBtn.addEventListener("click", closePopup);
+	popupAddBtn.addEventListener("click", changeTodoText);
+	todoInput.addEventListener("keyup", enterKeyCheck);
 };
 
 const addNewTodo = () => {
@@ -62,15 +78,48 @@ const checkClick = (e) => {
 	if (e.target.matches(".complete")) {
 		e.target.closest("li").classList.toggle("completed");
 		e.target.classList.toggle("completed");
-		console.log("completed");
-		console.log(e.target);
+		// console.log("completed");
+		// console.log(e.target);
 	} else if (e.target.matches(".edit")) {
-		console.log("edited");
-		console.log(e.target);
+		editTodo(e);
+		// console.log("edited");
+		// console.log(e.target);
 	} else if (e.target.matches(".delete")) {
-		console.log("deleted");
-		console.log(e.target);
+		deleteTodo(e);
+		// console.log("deleted");
+		// console.log(e.target);
 	}
 };
 
+const editTodo = (e) => {
+	todoToEdit = e.target.closest("li");
+	popupInput.value = todoToEdit.firstChild.textContent;
+	popup.style.display = "flex";
+};
+const closePopup = () => {
+	popup.style.display = "none";
+	popupInfo.textContent = "";
+};
+const changeTodoText = () => {
+	if (popupInput.value !== "") {
+		todoToEdit.firstChild.textContent = popupInput.value;
+		popup.style.display = "none";
+		popupInfo.textContent = "";
+	} else {
+		popupInfo.textContent = "Musisz podać jakąś treść!";
+	}
+};
+
+const deleteTodo = (e) => {
+	e.target.closest("li").remove();
+	const allTodos = ulList.querySelectorAll("li");
+	if (allTodos.length === 0) {
+		errorInfo.textContent = "Brak zadań na liście.";
+	}
+};
+const enterKeyCheck = (e) => {
+	if (e.key === "Enter") {
+		addNewTodo();
+	}
+};
 document.addEventListener("DOMContentLoaded", main);
